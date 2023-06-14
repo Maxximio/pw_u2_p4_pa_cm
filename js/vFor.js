99022458c8a7b0d3a4597389fbf891bb7cb2c44d
Carlos Montalvo <patriciosalas101@gmail.com>
2023-06-14 13:53:19 -0500

    tarea 6

--- js/vFor.js
+++ js/vFor.js
@@ -1,45 +1,11 @@
-var a = {
-  nombre: "Paulo",
-  apellido: "Aldaz",
-};
 var b = {
   nombre: "Carlos",
   apellido: "Montalvo",
-};
-var c = {
-  nombre: "Andy",
-  apellido: "Sanchez",
-};
-var d = {
-  nombre: "Monica",
-  apellido: "Andrade",
-};
-var e = {
-  nombre: "Pedro",
-  apellido: "Albuja",
-};
-var f = {
-  nombre: "Paul",
-  apellido: "Gonzalez",
-};
-var g = {
-  nombre: "Paulo2",
-  apellido: "Aldaz2",
-};
-var h = {
-  nombre: "Paulo3",
-  apellido: "Aldaz3",
-};
-var i = {
-  nombre: "Paulo4",
-  apellido: "Aldaz4",
-};
-var j = {
-  nombre: "Paulo5",
-  apellido: "Aldaz5",
+  edad: 23,
+  telefono: "0980673336"
 };
 
-const arreglo = [a, b, c, d, e, f, g, h, i, j];
+const arreglo = [b];
 
 const app = Vue.createApp({
   data() {
@@ -47,53 +13,32 @@ const app = Vue.createApp({
       miarreglo: arreglo,
 
       nombre: '',
+      apellido: '',
+      edad: null,
+      telefono: ''
     };
   },
   methods: {
-    agregarEstudaite(event) {
-      console.log("vamos a agrgar");
-      console.log(this.nombre);
-      console.log(event.charCode);
-      if (event.charCode == "13") {
-        console.log("presiono Enter");
-        const nuevoEstudiante = {
-          nombre: this.nombre,
-          apellido: "Solo"
-        };
-        this.miarreglo.unshift(nuevoEstudiante);//principio
-        
-      } else {
-        console.log("no presiono Enter");
-      }
-    },
 
-    agregarEstudaite2() {
-    console.log('Vamos a agregar')
-    const nuevoEstudiante={
-      nombre:this.nombre,
-      apellido: 'Solo'
-    }
-    this.miarreglo.push(nuevoEstudiante)//final
+    ingresar(){
+      
+      const nuevoEstudiante={
+        nombre:this.nombre,
+        apellido:this.apellido,
+        edad:this.edad,
+        telefono:this.telefono
+      }
+      this.miarreglo.push(nuevoEstudiante)
+        
+        this.nombre= '',
+        this.apellido= '',
+        this.edad= null,
+        this.telefono= ''
+
     }
   },
 });
 
-// function busqueda(miarreglo,nombre){
-//   if(miarreglo.nombre==nombre){
-//     return nombre;
-//   }else{
-//     busqueda(miarreglo[+1])
-//   }
-// }
-
 console.log(arreglo);
 
 app.mount("#miaplicacion2");
-
-// function insertarEstudiante(estudiante){
-
-// }
-
-// function insertarEstudiante({nombre}){
-
-// }
