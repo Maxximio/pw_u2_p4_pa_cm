0b9d01425b437026280ed92166b42895f49ab860
Carlos Montalvo <patriciosalas101@gmail.com>
2023-06-14 09:32:38 -0500

    tarea_5

---
+++ js/appAngular.js
@@ -0,0 +1,4 @@
+angular.module('myApp', [])
+  .controller('myController', function($scope) {
+    $scope.title = 'Hola Mundo desde Angular';
+  });
---
+++ js/appReact.js
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+      <div>
+        <h1>Hola Mundo desde React</h1>
+      </div>
+    );
+  };
+  
+  ReactDOM.render(<App />, document.getElementById('root'));
